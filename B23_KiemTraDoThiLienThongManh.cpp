d7ca6de33aa4414142e6b36aeb62c12ccacade82
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2022-07-10 17:10:12 +0700

    Update B23_KiemTraDoThiLienThongManh.cpp

--- B23_KiemTraDoThiLienThongManh.cpp
+++ B23_KiemTraDoThiLienThongManh.cpp
@@ -1,41 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 1001;
+vector<int>v[maxn] , r[maxn];
+bool ok[maxn];
+stack<int>st;
 
-vector<int>v[1001];
-int check[10001];
-
-void DFS(int u)
+void dfs1(int s)
 {
-    check[u] = 1;
-    for(int x : v[u])
+    ok[s] = true;
+    for(int x : v[s])
     {
-        if(!check[x])
+        if(!ok[x])
         {
-            DFS(x);
+            dfs1(x);
+        }
+    }
+    st.push(s);
+}
+void dfs2(int s)
+{
+    ok[s] = true;
+    for(int x : r[s])
+    {
+        if(!ok[x])
+        {
+            dfs2(x);
         }
     }
 }
-
-int main()
+void init()
 {
     int n , m ; cin >> n >> m;
     for(int i = 1 ; i <= m ; i++)
     {
         int a , b ; cin >> a >> b;
         v[a].push_back(b);
+        r[b].push_back(a);
     }
-    for(int i = 1 ; i <= n ; i++)
+    for(int i = 1 ; i <= n ;i++)
     {
-        memset(check , 0 , sizeof(check));
-        DFS(i);
-        for(int j = 1 ; j <= n ;j++)
+        if(!ok[i])
         {
-            if(!check[j])
-            {
-                cout << 0;
-                return 0;
-            }
+            dfs1(i);
         }
     }
-    cout << 1;
+    memset(ok , false , sizeof(ok));
+    int ans = 0;
+    while(!st.empty())
+    {
+        int top = st.top(); st.pop();
+        if(!ok[top])
+        {
+            dfs2(top);
+            ++ans;
+        }
+    }
+    if(ans == 1) cout << 1;
+    else cout << 0;
+}
+int main()
+{
+    init();
 }
